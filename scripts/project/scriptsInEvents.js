8d4ebd99f1bdcc8c8e5a785174d6ffd4cd314de8
Bitowsky2 <67092848+Bitowsky@users.noreply.github.com>
2024-05-25 09:33:13 +0200

    send

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,23 @@
+
+
+
+const scriptsInEvents = {
+
+	async E_display_Event21_Act2(runtime, localVars)
+	{
+		if(confirm("Pin-o-pedia will open the following link:\n'" + runtime.globalVars.TEMP_ARTISTLINK + "'\n\nContinue?") == true) {
+			window.open(runtime.globalVars.TEMP_ARTISTLINK, '_blank').focus();
+		}
+	},
+
+	async E_display_Event22_Act2(runtime, localVars)
+	{
+		if(confirm("Pin-o-pedia will open the following link:\n'" + runtime.globalVars.TEMP_ARTISTLINK + "'\n\nContinue?") == true) {
+			window.open(runtime.globalVars.TEMP_ARTISTLINK, '_blank').focus();
+		}
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
